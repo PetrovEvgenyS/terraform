e6cc67012383f908a86f9602397563f75f910398
Петров Евгений <PetrovE2@ya.ru>
2026-02-24 08:46:10 +0800

    Добавлена поддержка Ubuntu/Debian в скрипте установки Terraform
    - Определение ОС по /etc/os-release, apt для Ubuntu/Debian, dnf для AlmaLinux/RHEL и др.

--- !tools/install_terraform_almalinux.sh
+++ !tools/install_terraform_almalinux.sh
@@ -9,7 +9,7 @@ errorprint() { echo; printf "${RED}%s${RESET}\n" "$1"; }
 greenprint() { echo; printf "${GREEN}%s${RESET}\n" "$1"; }
 
 ### Установим переменные ###
-TF_VERSION="1.14.3"
+TF_VERSION="1.14.5"
 TF_URL="https://hashicorp-releases.yandexcloud.net/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip"
 
 # ---------------------------------------------------------------------------------------- #
@@ -20,9 +20,27 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Установка необходимых утилит
+# Определение ОС и установка утилит
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+else
+  errorprint "Не удалось определить ОС (/etc/os-release отсутствует)."
+  exit 1
+fi
+
 magentaprint "Устанавливаю необходимые утилиты: unzip, wget..."
-dnf install -y unzip wget
+case "$ID" in
+  almalinux|rhel|centos|fedora|rocky)
+    dnf install -y unzip wget
+    ;;
+  ubuntu|debian)
+    apt-get update -qq && apt-get install -y unzip wget
+    ;;
+  *)
+    errorprint "Неподдерживаемая ОС: $ID. Поддерживаются: AlmaLinux/RHEL/CentOS/Fedora/Rocky, Ubuntu/Debian."
+    exit 1
+    ;;
+esac
 
 # Загрузка и установка Terraform
 magentaprint "Загрузка Terraform версии $TF_VERSION..."
